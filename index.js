f67d78e29889f88e8c9ff3865097fddc4a54971f
ElieAzar-Algo <elie.azar3891.ea@gmail.com>
2020-10-22 14:29:39 +0300

    step 4

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+const e = require('express');
 // import express (after npm install express)
 const express = require('express');
+const { restart } = require('nodemon');
 
 // create new express app and save it as "app"
 const app = express();
@@ -19,6 +21,22 @@ app.get('/test', (req, res) => {
   app.get('/time', (req, res) => {
     res.status(200).send(date.getHours()+":"+date.getMinutes()+":"+date.getSeconds());
   });
+
+  app.get('/hello', (req, res) => {
+    res.status(200).send('Hello')
+   });
+
+  app.get('/hello/:id', (req, res) => {
+   res.status(200).send('Hello '+req.params.id)
+  });
+
+
+  app.get('/search', (req, res) => {
+  if (req.query.s==""||req.query.s==undefined){
+      res.status(500).send('you have to provide a search');
+  }else{
+    res.status(200).send('ok '+req.query.s)}
+   });
 
 
 // make the server listen to requests
