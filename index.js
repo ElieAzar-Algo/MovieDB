6cb212df2258dc258fc1b5866d00a097bbf63a0f
ElieAzar-Algo <elie.azar3891.ea@gmail.com>
2020-10-22 15:30:12 +0300

    step 5

--- index.js
+++ index.js
@@ -37,7 +37,30 @@ app.get('/test', (req, res) => {
   }else{
     res.status(200).send('ok '+req.query.s)}
    });
+   //-----------------------------basis for CRUD----------------------------------------------
+   const movies = [
+    { title: 'Jaws', year: 1975, rating: 8 },
+    { title: 'Avatar', year: 2009, rating: 7.8 },
+    { title: 'Brazil', year: 1985, rating: 8 },
+    { title: 'الإرهاب والكباب‎', year: 1992, rating: 6.2 }
+];  
+   
+   app.get('/movie/create', (req, res) => {
+    res.status(200).send(' ')
+   });
 
+   app.get('/movie/read', (req, res) => {
+      const data={d:movies}
+       res.status(200).send(data);
+       });
+
+   app.get('/movie/create', (req, res) => {
+    res.status(200).send(' ')
+   });
+
+   app.get('/movie/delete', (req, res) => {
+    res.status(200).send(' ')
+   });
 
 // make the server listen to requests
 app.listen(PORT, () => {
